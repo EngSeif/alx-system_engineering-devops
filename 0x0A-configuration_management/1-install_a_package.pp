e2122bb6b6e510a525bca06c51c9e9f7535b022b
EngSeif <eng.seifmohamed2004@gmail.com>
2024-06-28 21:33:49 +0300

    0x0A puppet | Fix Task

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,3 +3,8 @@ package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
+
+package { 'werkzeug':
+  ensure   => '2.0.2',  # Replace with the version compatible with Flask 2.1.0
+  provider => 'pip3',
+}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,10 +1,12 @@
-# Puppet manifest to install Flask version 2.1.0 using pip3
+# Install Flask version 2.1.0 using pip3
+
 package { 'pip3':
-  ensure => installed,
+  ensure   => installed,
+  provider => 'pip3',
 }
 
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => ['/usr/bin'],
-  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['pip3'],
 }
