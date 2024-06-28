e4912dbcfcb25b138321bba8ac617172fa89152f
EngSeif <eng.seifmohamed2004@gmail.com>
2024-06-28 20:56:54 +0300

    0x0A Puppet | Fix Task 2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,19 @@
-# kills a process named killmenow
-exec { 'kill_process':
-  command  =>  'pkill killmenow',
-  provider => 'shell',
+# Install Python 3.8 (if not already installed via system package manager)
+package { 'python3.8':
+  ensure   => installed,  # Ensure Python 3.8 is installed
+  provider => 'pip3',     # Use pip3 provider
+}
+
+# Install Flask version 2.1.0
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3.8'],  # Require Python 3.8 to be installed first
+}
+
+# Install Werkzeug version 2.1.1
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['flask'],  # Require Flask to be installed first
 }
