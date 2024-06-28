0fb13c002b52f85ef3df1e6e676b01b799507039
EngSeif <eng.seifmohamed2004@gmail.com>
2024-06-28 21:04:47 +0300

    0x0A Puppet | Fix Task 2 | v4

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,10 +1,6 @@
-#install flask with pip3
-package { 'pip3':
-  ensure => installed,
-}
-
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => ['/usr/bin', '/usr/local/bin'],
-  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+#!/usr/bin/pup
+# Install an especific version of flask (2.1.0)
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
