ff25e42930cc55878b5b115324d748f38eec44a5
EngSeif <eng.seifmohamed2004@gmail.com>
2024-07-29 23:35:04 +0300

    API | get info | fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,27 +1,30 @@
 #!/usr/bin/python3
 """ Get Employee info """
-import requests
-from sys import argv
 
-if len(argv) < 2:
-    exit(1)
+if __name__ == "__main__":
+    import requests
+    from sys import argv
 
-id = argv[1]
-url_info = f"https://jsonplaceholder.typicode.com/users/{id}"
-url_toDo = f"https://jsonplaceholder.typicode.com/users/{id}/todos"
+    if len(argv) < 2:
+        exit(1)
 
-r = requests.get(url_info)
-rd = requests.get(url_toDo)
+    id = argv[1]
+    url_info = f"https://jsonplaceholder.typicode.com/users/{id}"
+    url_toDo = f"https://jsonplaceholder.typicode.com/users/{id}/todos"
 
-content_info = r.json()
-content_todo = rd.json()
-name = content_info.get('name')
-done_tasks = 0
-for item in content_todo:
-    if item.get('completed'):
-        done_tasks += 1
-print(f"Employee {name} is done with tasks({done_tasks}/{len(content_todo)}):")
-for item in content_todo:
-    if item.get('completed'):
-        task_name = item.get('title')
-        print(f"\t{task_name}")
+    r = requests.get(url_info)
+    rd = requests.get(url_toDo)
+
+    content_info = r.json()
+    content_todo = rd.json()
+    name = content_info.get('name')
+    do_tasks = 0
+    for item in content_todo:
+        if item.get('completed'):
+            do_tasks += 1
+    s = f"Employee {name} is done with tasks({do_tasks}/{len(content_todo)}):"
+    print(s)
+    for item in content_todo:
+        if item.get('completed'):
+            task_name = item.get('title')
+            print(f"\t{task_name}")
