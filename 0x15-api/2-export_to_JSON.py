757afebbd9f2b68d5c47b40f4abe9c2aebc4bf9a
EngSeif <eng.seifmohamed2004@gmail.com>
2024-07-30 00:10:40 +0300

    fix style

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     content_info = r.json()
     content_todo = rd.json()
     user_id = content_info.get('id')
-    
+
     file_name = f"{user_id}.json"
     data = {}
     user_id = content_info.get('id')
@@ -27,11 +27,11 @@ if __name__ == "__main__":
     data[user_id] = []
     user_tasks = [t for t in content_todo if t['userId'] == user_id]
     for task in user_tasks:
-            task_info = {
-                "task": task.get('title'),
-                "completed": task.get('completed'),
-                "username": u_name
+        task_info = {
+            "task": task.get('title'),
+            "completed": task.get('completed'),
+            "username": u_name
             }
-            data[user_id].append(task_info)
+        data[user_id].append(task_info)
     with open(file_name, mode='w') as file:
         json.dump(data, file)
