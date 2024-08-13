5e38e7ac9e39d353b457b90ad741d26665f081db
EngSeif <eng.seifmohamed2004@gmail.com>
2024-08-13 22:26:57 +0300

    API Advanced | Number of Subs | fix V4

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,9 +11,9 @@ def number_of_subscribers(subreddit):
     e a function that queries the Reddit API and
     returns the number of subscribers
     """
-    url = f"https://api.reddit.com/r/{subreddit}/about/"
-    headers = {'User-Agent': 'my-custom-user-agent'}
-    r = requests.get(url, headers=headers,allow_redirects=False)
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'request'}
+    r = requests.get(url, headers=headers, allow_redirects=False)
     if r.status_code == 200:
         data = r.json().get("data", {})
         return data.get("subscribers", 0)
