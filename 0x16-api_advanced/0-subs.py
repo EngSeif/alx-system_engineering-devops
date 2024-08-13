903a4202d709ba7d18d726fc30b9a0ec6017d40f
EngSeif <eng.seifmohamed2004@gmail.com>
2024-08-13 22:05:42 +0300

    API Advanced | Number of Subs | fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,6 +5,7 @@ queries the Reddit API and returns the number of subscribers
 """
 import requests
 
+
 def number_of_subscribers(subreddit):
     """
     e a function that queries the Reddit API and
